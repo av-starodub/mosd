1f7648ca5434f8d2fc6de2c0ae58cffe0e6f0e8d
Alexandr Starodubenko <alexvladstr@gmail.com>
2025-10-28 23:56:02 +0300

    Initialize a Spring Boot application with basic structure, dependencies, and initial configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
+val springdocVersion by extra("2.8.13")
+val commonsLang3Version by extra("3.19.0") // CVE-2025-48924 vulnerability in versions < 3.18.0 https://www.mend.io/vulnerability-database/CVE-2025-48924?utm_source=JetBrains
+val checkstyleVersion by extra("12.1.1")
+
 plugins {
     java
     checkstyle
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "io.github.avstarodub"
@@ -15,11 +21,37 @@ repositories {
     mavenCentral()
 }
 
+dependencyManagement {
+    imports {
+        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+    }
+    dependencies {
+        dependency("org.apache.commons:commons-lang3:$commonsLang3Version")
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
 checkstyle {
-    toolVersion = "10.12.5"
+    toolVersion = checkstyleVersion
     configFile = file("config/checkstyle/checkstyle.xml")
     isIgnoreFailures = false
     maxWarnings = 0
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging.showExceptions = true
+    reports {
+        junitXml.required.set(true)
+        html.required.set(true)
+    }
 }
 
 tasks.named<Checkstyle>("checkstyleMain") {
---
+++ src/main/java/io/github/avstarodub/mosd/MosdApplication.java
@@ -0,0 +1,13 @@
+package io.github.avstarodub.mosd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosdApplication.class, args);
+	}
+
+}
